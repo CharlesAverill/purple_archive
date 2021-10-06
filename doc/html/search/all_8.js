c993b7721a8aef5b75797611e1df5457d52a14df
Charles Averill <charlesaverill20@gmail.com>
2021-10-05 19:21:33 -0500

    scan.c/scan

--- doc/html/search/all_8.js
+++ doc/html/search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['scan_2ec_0',['scan.c',['../scan_8c.html',1,'']]],
-  ['skip_1',['skip',['../scan_8c.html#ae1cbaaa04222c8794e6fb52ab9a0078d',1,'scan.c']]]
+  ['scan_0',['scan',['../scan_8c.html#ad92e6905738891696232b05133dbf8fc',1,'scan.c']]],
+  ['scan_2ec_1',['scan.c',['../scan_8c.html',1,'']]],
+  ['skip_2',['skip',['../scan_8c.html#ae1cbaaa04222c8794e6fb52ab9a0078d',1,'scan.c']]]
 ];
--- doc/html/search/functions_2.js
+++ doc/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['skip_0',['skip',['../scan_8c.html#ae1cbaaa04222c8794e6fb52ab9a0078d',1,'scan.c']]]
+  ['scan_0',['scan',['../scan_8c.html#ad92e6905738891696232b05133dbf8fc',1,'scan.c']]],
+  ['skip_1',['skip',['../scan_8c.html#ae1cbaaa04222c8794e6fb52ab9a0078d',1,'scan.c']]]
 ];
